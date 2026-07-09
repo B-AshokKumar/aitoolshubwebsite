d64527fd13a2c37640760b3b227e3ad6c8d6edb0
B-AshokKumar <ashokashkashas@gmail.com>
2026-07-09 16:19:39 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("AI Tools Hub Website Loaded");
+document
+.getElementById("searchBtn")
+.addEventListener("click",function(){
+
+let keyword=document
+.querySelector("input")
+.value;
+
+if(keyword==""){
+
+alert("Please enter AI tool name.");
+
+return;
+
+}
+
+alert("Searching for: "+keyword);
+
+});
